f48a324098c52f4406ef5f9b166b96442f2fe8ea
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-10-12 13:45:57 +0200

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+
+
+fun main() {
+
+    var lista = listOf(Gato(2), Perro(20), Persona(89),Gato(3), Perro(15), Persona(79),Gato(3), Perro(25), Persona(99))
+
+
+    // Haz un ejercicio como output tenga:
+    // El peso medio de los gatos es:
+    // El peso medio de los perros es:
+    // El peso medio de las personas es:
+}
